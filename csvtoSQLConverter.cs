282803d45236b7a881d70159bb3023318202d5ec
Jake Deakin <jake.deakin@outlook.com>
2023-02-18 15:45:16 +1000

    Renamed program to csvtoSQLConverter. Added a .csv file to test with. Added in hard coded filepaths for simplicity while testing. Need to be changed per user.

---
+++ csvtoSQLConverter.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel;
+using System.Data.Common;
+
+String sqlInsert;
+String filePath = "C:\\Users\\Jake\\Documents";
+String filePathtoOpen = "C:\\Users\\Jake\\Documents\\TestCSv.csv";
+String fileNametoSave;
+String dbTableName;
+
+//Console.WriteLine("File path of csv file?");
+//filePathtoOpen = Console.ReadLine();
+
+Console.WriteLine("Table Name?");
+dbTableName = Console.ReadLine();
+
+string firstLine = File.ReadLines(filePathtoOpen).First();
+sqlInsert = "Insert into " + dbTableName + " (" + firstLine + "),\n";
+
+foreach (string csvLine in File.ReadLines(filePathtoOpen).Skip(0))
+{
+    sqlInsert += "(" + csvLine + ")," + "\n";
+}
+sqlInsert = sqlInsert.Remove(sqlInsert.Length - 2) + ";";
+sqlInsert = sqlInsert.Replace("\"", "");
+
+Console.WriteLine(sqlInsert);
+Console.WriteLine("What would you like to name the query?");
+fileNametoSave = Console.ReadLine();
+File.WriteAllText(filePath + "\\" + fileNametoSave, sqlInsert);
+
