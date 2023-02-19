ba3336c0bb426ff4d227258c0fa1eaf457460db3
Jake Deakin <jake.deakin@outlook.com>
2023-02-19 17:02:23 +1000

    Changing the program to use classes and a Main() method.

--- csvtoSQLConverter.cs
+++ csvtoSQLConverter.cs
@@ -1,80 +1,97 @@
-﻿String sqlInsert = "Insert into ";
-String filePath = "C:\\Users\\Jake\\Documents\\sqlOutput";
-String filePathtoOpen = "C:\\Users\\Jake\\Documents\\TestCSv.csv";
-String dbTableName = "Table";
-Boolean validFileName = false;
-Boolean validFilePath = false;
-
-do
+﻿public class csvtoSQLConverter
 {
-    try
+    String sqlInsert = "Insert into ";
+    static String filePath = "C:\\Users\\Jake\\Documents\\sqlOutput";
+    static String filePathtoOpen = "C:\\Users\\Jake\\Documents\\test.csv";
+    String dbTableName = "Table";
+    Boolean validFileName = false;
+    Boolean validFilePath = false;
+
+
+    public static void Main(String[] args)
     {
-        if (filePathtoOpen == String.Empty || filePathtoOpen == null)
-        {
-            Console.WriteLine("File path of csv file? (including name of file)");
-            filePathtoOpen = Console.ReadLine() ?? String.Empty;
-        }
-
-        if (filePathtoOpen == null)
-        {
-            throw new Exception("File path is null");
-        }
-
-        string firstLine = File.ReadLines(filePathtoOpen).First();
-
-        validFilePath = true;
-
-        Console.WriteLine("Table Name?");
-        dbTableName = Console.ReadLine() ?? String.Empty;
-
-        sqlInsert = "Insert into " + dbTableName + " (" + firstLine + "),\n";
-
-        foreach (string csvLine in File.ReadLines(filePathtoOpen).Skip(0))
-        {
-            sqlInsert += "(" + csvLine + ")," + "\n";
-        }
-        sqlInsert = sqlInsert.Remove(sqlInsert.Length - 2) + ";";
-        sqlInsert = sqlInsert.Replace("\"", "");
-
-        Console.WriteLine(sqlInsert);
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine(ex.Message);
-        Console.WriteLine("Please enter a valid file path.");
-        filePathtoOpen = String.Empty;
+        csvtoSQLConverter converter = new csvtoSQLConverter();
+        converter.convertCSVtoSQLFile(filePathtoOpen, filePath);
+        
     }
 
-} while (!validFilePath);
-
-do
-{
-    try
+    public void convertCSVtoSQLFile(String filePathtoOpen, String filePathtoSave)
     {
-        if (filePath == String.Empty || filePath == null)
+        do
         {
-            Console.WriteLine("Where would you like to save the query? (file name inclusive)");
+            try
+            {
+                if (filePathtoOpen == String.Empty || filePathtoOpen == null)
+                {
+                    Console.WriteLine("File path of csv file? (including name of file)");
+                    filePathtoOpen = Console.ReadLine() ?? String.Empty;
+                }
 
-            filePath = Console.ReadLine() ?? String.Empty;
-        }
+                if (filePathtoOpen == null)
+                {
+                    throw new Exception("File path is null");
+                }
 
-        if (filePath == null)
+                string firstLine = File.ReadLines(filePathtoOpen).First();
+
+                validFilePath = true;
+
+                Console.WriteLine("Table Name?");
+                dbTableName = Console.ReadLine() ?? String.Empty;
+
+                sqlInsert = "Insert into " + dbTableName + " (" + firstLine + "),\n";
+
+                foreach (string csvLine in File.ReadLines(filePathtoOpen).Skip(0))
+                {
+                    sqlInsert += "(" + csvLine + ")," + "\n";
+                }
+                sqlInsert = sqlInsert.Remove(sqlInsert.Length - 2) + ";";
+                sqlInsert = sqlInsert.Replace("\"", "");
+
+                Console.WriteLine(sqlInsert);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Please enter a valid file path.");
+                filePathtoOpen = String.Empty;
+            }
+
+        } while (!validFilePath);
+
+        do
         {
-            throw new Exception("File path is null");
-        }
-        File.WriteAllText(filePath, sqlInsert);
-        validFileName = true;
-    }
-    catch (Exception ex)
-    {
-        Console.WriteLine(ex.Message);
-        Console.WriteLine("You must give a valid file path for the query to be saved.");
-        filePath = String.Empty;
-    }
-   
-} while (!validFileName);
+            try
+            {
+                if (filePathtoSave == String.Empty || filePath == null)
+                {
+                    Console.WriteLine("Where would you like to save the query? (file name inclusive)");
 
-Console.WriteLine("File saved to " + filePath);
+                    filePathtoSave = Console.ReadLine() ?? String.Empty;
+                }
+
+                if (filePathtoSave == null)
+                {
+                    throw new Exception("File path is null");
+                }
+                File.WriteAllText(filePathtoSave, sqlInsert);
+                validFileName = true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("You must give a valid file path for the query to be saved.");
+                filePathtoSave = String.Empty;
+            }
+
+        } while (!validFileName);
+
+        Console.WriteLine("File saved to " + filePathtoSave);
+    }
+
+}
+
+
 
 
 
